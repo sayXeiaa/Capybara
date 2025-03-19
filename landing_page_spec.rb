8d0d1b7084c956206c9f03fd0469f53d7e4ff868
Robin Joshua R. Hermocilla <124029762+sayXeiaa@users.noreply.github.com>
2025-03-19 09:46:58 +0800

    created more examples

--- landing_page_spec.rb
+++ landing_page_spec.rb
@@ -9,14 +9,31 @@ RSpec.describe "Landing Page", type: :feature do
   end
 
   before(:each) do
-    landing_page.load
+    landing_page.visit_landing_page
   end
+
   context "Checking Landing Page UI" do
     it "Displays the Home label" do
       expect(landing_page.home_label.text).to eq("Home")
     end
     it "Has a Logo" do
       expect(landing_page).to have_logo
+    end
+    it "Has a real-time tracking section" do
+      expect(landing_page).to have_tracking_section
+    end
+    it "has a graphical analytics section" do
+      expect(landing_page).to have_analytics_section
+    end
+    it "has a user-friendly section" do
+      expect(landing_page).to have_user_friendly_section
+    end
+    it "Displays the tournament button" do
+      expect(landing_page).to have_tournament_button
+    end
+    it "Has a clickable tournament button" do
+      landing_page.tournament_button_clickable?
+      expect(landing_page).to have_current_path("/tournaments")
     end
   end
 end
